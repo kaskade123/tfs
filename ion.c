c76310b198bd315678b77575c1e731b2c5d6c29f
Ke Pan <pppeterpppan@gmail.com>
2015-08-22 14:13:50 +0800

    Minimize stack usage in ion module

--- ion.c
+++ ion.c
@@ -4,20 +4,20 @@
 
 typedef struct iom_status
 {
+	int ionFd;
+	ION_PKT_S RECV_PKT, SEND_PKT;
+	BOOL ionInited;
 	UINT32 RESETS;
 	UINT32 TEMPERATURE;
 	UINT32 pktSent;
 	UINT32 pktRecv;
 }IOM_STATUS_S;
 
-static int ionFd;
-static ION_PKT_S RECV_PKT, SEND_PKT;
-static BOOL ionInited = FALSE;
-static IOM_STATUS_S status;
+static IOM_STATUS_S * pStatus = NULL;
 
 static ION_PKT_S * ionHook(UINT8 type)
 {
-	return &RECV_PKT;
+	return &pStatus->RECV_PKT;
 }
 #ifdef DISPLAY
 static void ion_pkt_display(void * arg, char * desc)
@@ -37,83 +37,84 @@ static void ion_pkt_display(void * arg, char * desc) {}
 
 static void ion_send_statistics_check(void)
 {
-	assert(ionInited);
+	assert(pStatus->ionInited);
 	
 	/* 0x02 0x40 CRC */
-	SEND_PKT.PRI = 6;
-	SEND_PKT.RP = 0;
-	SEND_PKT.DST = IO_ADDR;
-	SEND_PKT.DLC = 4;
-	SEND_PKT.pkt_buf[0] = 3;
-	SEND_PKT.pkt_buf[1] = 0x49;
-	SEND_PKT.pkt_buf[2] = 1;
-	SEND_PKT.pkt_buf[3] = 0x00; /* CRC ignored if packet can degration */
+	pStatus->SEND_PKT.PRI = 6;
+	pStatus->SEND_PKT.RP = 0;
+	pStatus->SEND_PKT.DST = IO_ADDR;
+	pStatus->SEND_PKT.DLC = 4;
+	pStatus->SEND_PKT.pkt_buf[0] = 3;
+	pStatus->SEND_PKT.pkt_buf[1] = 0x49;
+	pStatus->SEND_PKT.pkt_buf[2] = 1;
+	pStatus->SEND_PKT.pkt_buf[3] = 0x00; /* CRC ignored if packet can degration */
 	
-	ion_pkt_display(&SEND_PKT, "Send");
+	ion_pkt_display(&pStatus->SEND_PKT, "Send");
 	
-	assert(IONPktSend(ionFd, &SEND_PKT) == 0);
+	assert(IONPktSend(pStatus->ionFd, &pStatus->SEND_PKT) == 0);
 	
-	status.pktSent++;
+	pStatus->pktSent++;
 }
 
 static void ion_decode_statistics_check(void)
 {
-	if (RECV_PKT.DLC < 61)
+	if (pStatus->RECV_PKT.DLC < 61)
 		return;
 	
-	status.pktRecv++;
+	pStatus->pktRecv++;
 	
-	memcpy(&status.RESETS, RECV_PKT.pkt_buf + 56, 4);
+	memcpy(&pStatus->RESETS, pStatus->RECV_PKT.pkt_buf + 56, 4);
 	
 	/* little endian to big endian convert */
-	status.RESETS = (status.RESETS & 0x000000FF) << 24 ||
-					(status.RESETS & 0x0000FF00) << 8  ||
-					(status.RESETS & 0x00FF0000) >> 8  ||
-					(status.RESETS & 0xFF000000) >> 24;
+	pStatus->RESETS = (pStatus->RESETS & 0x000000FF) << 24 ||
+					  (pStatus->RESETS & 0x0000FF00) << 8  ||
+					  (pStatus->RESETS & 0x00FF0000) >> 8  ||
+					  (pStatus->RESETS & 0xFF000000) >> 24;
 }
 
 static void ion_send_temp_check(void)
 {
-	assert(ionInited);
+	assert(pStatus->ionInited);
 	
 	/* 0x02 0x40 CRC */
-	SEND_PKT.PRI = 6;
-	SEND_PKT.RP = 0;
-	SEND_PKT.DST = IO_ADDR;
-	SEND_PKT.DLC = 3;
-	SEND_PKT.pkt_buf[0] = 2;
-	SEND_PKT.pkt_buf[1] = 0x63;
-	SEND_PKT.pkt_buf[2] = 0x00; /* CRC is ignored by driver due to degration */
+	pStatus->SEND_PKT.PRI = 6;
+	pStatus->SEND_PKT.RP = 0;
+	pStatus->SEND_PKT.DST = IO_ADDR;
+	pStatus->SEND_PKT.DLC = 3;
+	pStatus->SEND_PKT.pkt_buf[0] = 2;
+	pStatus->SEND_PKT.pkt_buf[1] = 0x63;
+	pStatus->SEND_PKT.pkt_buf[2] = 0x00; /* CRC is ignored by driver due to degration */
 	
-	ion_pkt_display(&SEND_PKT, "Send");
+	ion_pkt_display(&pStatus->SEND_PKT, "Send");
 	
-	assert(IONPktSend(ionFd, &SEND_PKT) == 0);
+	assert(IONPktSend(pStatus->ionFd, &pStatus->SEND_PKT) == 0);
 	
-	status.pktSent++;
+	pStatus->pktSent++;
 }
 
 static void ion_decode_temp_check(void)
 {
-	if (RECV_PKT.DLC < 4)
+	if (pStatus->RECV_PKT.DLC < 4)
 		return;
-	status.pktRecv++;
-	status.TEMPERATURE = RECV_PKT.pkt_buf[3];
+	pStatus->pktRecv++;
+	pStatus->TEMPERATURE = pStatus->RECV_PKT.pkt_buf[3];
 }
 
 static int polling_task(void)
 {
 	INT32 ret;
-	assert(ionInited);
+	assert(pStatus->ionInited);
 	
 	FOREVER
 	{
 		do
 		{
 			/* Receive the packet that sent to us */
-			ret = IONPktPoll(ionFd);
-			if (((ret == 0) || (ret == -ENOSPC)) && (RECV_PKT.DST == addr_get()))
+			ret = IONPktPoll(pStatus->ionFd);
+			if (((ret == 0) || (ret == -ENOSPC)) &&
+					(pStatus->RECV_PKT.DST == addr_get()))
 			{
-				switch(RECV_PKT.pkt_buf[1])
+				switch(pStatus->RECV_PKT.pkt_buf[1])
 				{
 				case 0x05:
 					ion_decode_statistics_check();
@@ -124,7 +125,7 @@ static int polling_task(void)
 				default:
 					break;
 				}
-				ion_pkt_display(&RECV_PKT, "Recv");
+				ion_pkt_display(&pStatus->RECV_PKT, "Recv");
 			}
 		}while(ret != -EAGAIN);
 		taskDelay(1);
@@ -134,28 +135,30 @@ static int polling_task(void)
 static void ion_init(void)
 {
 	/* Only init once */
-	if (ionInited)
+	if (pStatus && pStatus->ionInited)
 		return;
 	
+	/* Malloc status struct */
+	pStatus = malloc(sizeof(*pStatus));
+	assert (pStatus);
+	memset(pStatus, 0, sizeof(*pStatus));
+	
     /* Request handler */
-	ionFd = iondev_get();
-	assert (ionFd >= 0);
+	pStatus->ionFd = iondev_get();
+	assert (pStatus->ionFd >= 0);
 	
 	/* Malloc paket buffer */
-	SEND_PKT.pkt_buf = malloc(256);
-	assert(SEND_PKT.pkt_buf != NULL);
+	pStatus->SEND_PKT.pkt_buf = malloc(256);
+	assert(pStatus->SEND_PKT.pkt_buf != NULL);
 
-	RECV_PKT.pkt_buf = malloc(256);
-	assert(RECV_PKT.pkt_buf != NULL);
-	
-	/* Initialize status struct */
-	memset(&status, 0, sizeof(status));
+	pStatus->RECV_PKT.pkt_buf = malloc(256);
+	assert(pStatus->RECV_PKT.pkt_buf != NULL);
 	
 	/* Hook the recevice function */
-	assert(IONHookRegister(ionFd, ionHook) == 0);
+	assert(IONHookRegister(pStatus->ionFd, ionHook) == 0);
 	
 	/* Init done */
-	ionInited = TRUE;
+	pStatus->ionInited = TRUE;
 	
 	/* Start polling task */
 	taskSpawn("tIONPoll", 253, 0, 0x40000, polling_task, 0,0,0,0,0,0,0,0,0,0);
@@ -195,10 +198,10 @@ void ion_show(char * buf)
 			"Packet Sent            : %u\n"
 			"Packet Recv            : %u\n"
 			"Packet Missing         : %u\n",
-			status.RESETS,
-			status.TEMPERATURE,
-			status.pktSent,
-			status.pktRecv,
-			status.pktSent - status.pktRecv
+			pStatus->RESETS,
+			pStatus->TEMPERATURE,
+			pStatus->pktSent,
+			pStatus->pktRecv,
+			pStatus->pktSent - pStatus->pktRecv
 			);
 }
