051b3279bb8885d935bd5fe0702e4a93f4a3bb58
Ke Pan <pppeterpppan@gmail.com>
2015-08-28 15:34:17 +0800

    Repeatly update module output.
    The test_show only do logMsg.

--- test.c
+++ test.c
@@ -6,6 +6,30 @@ MODULE_DECLARE(ion);
 MODULE_DECLARE(func);
 MODULE_DECLARE(eth);
 
+static char print_buf[2048];
+
+static int test_show_entry(int delay)
+{
+	/* Default to 10 seconds */
+	if (delay <= 0)
+		delay = 10;
+	
+	/* Save all task output to print_buf repeatly */
+	FOREVER
+	{
+		memset(print_buf, 0, 2048);
+		lib_show(print_buf);
+		taskDelay(delay * sysClkRateGet());
+	}
+	
+	return 0;
+}
+
+static void lib_show_start(int delay)
+{
+	taskSpawn("tShow", 254, VX_SPE_TASK, 0x100000, test_show_entry, delay,0,0,0,0,0,0,0,0,0);
+}
+
 static int test_start_entry(void)
 {	
 	/* initialize lib */
@@ -27,6 +51,9 @@ static int test_start_entry(void)
 	/* modules start*/
 	lib_start();
 	
+	/* show start */
+	lib_show_start(10);
+	
 	/* Last stage lib init */
 	lib_last_stage_init();
 	return 0;
@@ -37,32 +64,8 @@ void test_start(void)
 	taskSpawn("tStart", 255, VX_SPE_TASK, 0x100000, test_start_entry, 0,0,0,0,0,0,0,0,0,0);
 }
 
-static int test_show_entry(int delay)
+void test_show(void)
 {
-	char print_buf[2048];
-	
-	if (delay > 0 && delay < 10)
-	{
-		FOREVER
-		{
-			memset(print_buf, 0, 2048);
-			lib_show(print_buf);
-			logMsg(print_buf, 0,0,0,0,0,0);
-			taskDelay(delay * sysClkRateGet());
-		}
-	}
-	else
-	{
-		memset(print_buf, 0, 2048);
-		lib_show(print_buf);
-		logMsg(print_buf, 0,0,0,0,0,0);
-	}
-	
-	return 0;
-}
-
-void test_show(int delay)
-{
-	taskSpawn("tShow", 254, VX_SPE_TASK, 0x100000, test_show_entry, delay,0,0,0,0,0,0,0,0,0);
+	logMsg(print_buf, 0,0,0,0,0,0);
 }
 
