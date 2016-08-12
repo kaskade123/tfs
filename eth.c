9509be789ff19d4d0ceaf089ee2e1930fa72abf9
Ke Pan <pppeterpppan@gmail.com>
2016-08-12 20:12:02 +0800

    eth : Use cksum to ensure the packet is identical

--- eth.c
+++ eth.c
@@ -13,6 +13,7 @@ typedef struct eth_status
 	BOOL 	ethInited;
 	INT32 	hdr[ETH_DEV_COUNT];		/* Ethernet device handler */
 	UINT8 * pkt[ETH_DEV_COUNT]; 	/* Ethernet packet buffer */
+	UINT32 	pktCksum[ETH_DEV_COUNT];/* Ethernet packet cksum */
 	UINT32 	pktSent[ETH_DEV_COUNT]; /* Ethernet packet sent */
 	UINT32 	pktFail[ETH_DEV_COUNT]; /* Ethernet packet send fail */
 	UINT32 	pktRecv[ETH_DEV_COUNT];	/* Ethernet packet received */
@@ -27,11 +28,18 @@ static ETH_STATUS_S * pStatus = NULL;
 static BOOL eth_counting_hook(void * pDev, UINT8 *pBuf, UINT32 bufLen)
 {
 	ETHERNET_DEV_S * p = pDev;
+	UINT32 cksum, idx;
+	
+	calc_fletcher32(pBuf, bufLen, &cksum);
 	
 	/* Hook for eth1 - eth4 */
 	if ((strlen(p->name) == 4) && 
 			(strncmp(p->name, ETH_DEV_PREFIX, strlen(ETH_DEV_PREFIX)) == 0))
-		pStatus->pktRecv[p->name[strlen(ETH_DEV_PREFIX)] - '1'] ++;
+	{
+	    idx = p->name[strlen(ETH_DEV_PREFIX)] - '1';
+	    if (cksum == pStatus->pktCksum[idx])
+	        pStatus->pktRecv[idx] ++;
+	}
     return TRUE;
 }
 
@@ -49,7 +57,7 @@ static void eth_srcmac_fill(INT32 hdr, UINT8 * pkt)
         pkt[6+i] = mac32[i];
 }
 
-static int eth_send_random(INT32 hdr, UINT8 * pkt, UINT32 pkt_len)
+static int eth_send_random(INT32 hdr, UINT8 * pkt, UINT32 pkt_len, UINT32 * cksum)
 {
     int i;
     
@@ -67,6 +75,8 @@ static int eth_send_random(INT32 hdr, UINT8 * pkt, UINT32 pkt_len)
     /* fillup random stuff */
     for (i = 14; i < pkt_len; i++)
         pkt[i] = rand();
+    
+    calc_fletcher32(pkt, pkt_len, cksum);
 
     return EthernetSendPkt(hdr, pkt, pkt_len);
 }
@@ -77,7 +87,7 @@ static void eth_send_task(void * arg)
 	/* Send out one packet for each port */
 	for (i = 0; i < ETH_DEV_COUNT; i++)
 	{
-		if (eth_send_random(pStatus->hdr[i], pStatus->pkt[i], ETH_PKT_LEN))
+		if (eth_send_random(pStatus->hdr[i], pStatus->pkt[i], ETH_PKT_LEN, &pStatus->pktCksum[i]))
 			pStatus->pktFail[i]++;
 		else
 			pStatus->pktSent[i]++;
